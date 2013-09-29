a499a0808ab6e5c17fa0146840618f92ba4e3027
Bob Williams <bobwilliams.ii@gmail.com>
2013-09-29 11:14:10 -0400

    initial add

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 clojure-utils
 =============
 
-My own little utils library for clojure
+My own little utils library for clojure.
+
+I know there are other and better libraries out there but this is one which I have built as part of a the clolure learning process.
---
+++ clojure.utils.clj
@@ -0,0 +1,31 @@
+; math related functions
+
+(def fibo (map first (iterate (fn [[a b]] [b (+ a b)]) [0 1])))
+
+(defn factor? [n d] 
+	(if (zero? d)
+		false
+		(zero? (mod n d))))
+
+(defn gcd [a b] 
+  (if (zero? b) 
+    a 
+    (recur b (mod a b))))
+
+(defn lcm [a b] 
+  (/ (* a b) (gcd a b)))
+
+(defn square [n]
+  (* n n))
+
+(defn sum-of-squares [n]
+  (reduce + (map #(square %) (range 1 (+ n 1)))))
+
+(defn square-of-sums [n]
+  (square (reduce + (range 1 (+ n 1)))))
+
+(defn prime? [n]
+  (cond
+    (< n 2) false
+    (< n 4) true
+    :else (not= 0 (reduce *' (map #(mod n %) (range 2 (+ 1 (Math/sqrt n))))))))
