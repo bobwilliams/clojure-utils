5912ec4851b8ebe298a0aa79ffc9a602e7040973
Bob Williams <bobwilliams.ii@gmail.com>
2013-09-30 22:59:14 -0400

    adding pythagorean-triple

--- clojure.utils.clj
+++ clojure.utils.clj
@@ -2,11 +2,6 @@
 
 (def fibo (map first (iterate (fn [[a b]] [b (+ a b)]) [0 1])))
 
-(defn factor? [n d] 
-	(if (zero? d)
-		false
-		(zero? (mod n d))))
-
 (defn gcd [a b] 
   (if (zero? b) 
     a 
@@ -24,18 +19,25 @@
 (defn square-of-sums [n]
   (square (reduce + (range 1 (+ n 1)))))
 
+(defn factor? [n d] 
+  (if (zero? d)
+    false
+    (zero? (mod n d))))
+
 (defn prime? [n]
   (cond
     (< n 2) false
     (< n 4) true
     :else (not= 0 (reduce *' (map #(mod n %) (range 2 (+ 1 (Math/sqrt n))))))))
 
-
+(defn pythagorean-triplet? [a b c]
+  (true? (and (= (< a b) (< b c))
+     (= (+ (square a) (square b)) (square c)))))
 
 ; string related functions
 
 (use '[clojure.string :only (join split capitalize lower-case)])
 
 (defn title-case [s]
-  "Given a sentence, returns a string with each word captialized and all other letters lower-case."
+  "Given a string, returns a string with each word (separated by a space) captialized and all other letters lower-case."
   (join " " (map #(capitalize  %) (split (lower-case s) #"\s"))))
